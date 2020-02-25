1b7b5bacbe4b7e0c58b526c783b15964cc49ebb3
FrancescoAmorosini <francesco.amorosini@stud.unifi.it>
2020-02-25 12:59:42 +0100

    Basic login interface

---
+++ src/components/secure.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="secure">
+        <h1>User Profile</h1>
+        <p>
+            This is page is intended for users
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Secure',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+    #secure {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
