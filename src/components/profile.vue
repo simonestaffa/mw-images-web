7d92480e60b2be1d0bf289bdfc724b1ebaf5c51e
simonestaffa <simone.staffa@tutored.it>
2020-03-05 18:10:57 +0100

    File conversion to base64

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -2,55 +2,65 @@
     <div id="profile">
         <h1>Hello {{ res.first_name }}!</h1>
         <body>
-           TODO: add user infos and functionalities
-           <input type="file" accept="image/*" @change="uploadImage" id="file-input">
-           <router-link to="/signup" replace>Logout</router-link>
+        TODO: add user infos and functionalities
+        <input type="file" accept="image/*" @change="uploadImage" id="file-input">
+        <router-link to="/signup" replace>Logout</router-link>
         </body>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Profile',
-        data() {
-            return {
-                id:'',
-                token:'',
-                res:{}
-            };
-        },
-        mounted(){
-            this.token = localStorage.getItem('user-token')
-            this.id = this.$route.params.id
-            const axios = require('axios');
-            axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`
-            console.log(this.id);
-            axios
-                .get('http://0.0.0.0:5000/users/' + this.id)
-                .then(response => (
-                    this.res = response.data,
-                    console.log(JSON.stringify(response))
-                    ))
-                .catch(error => (console.log(error)));
-        },
-        methods: {
-            uploadImage(event) {
-                const axios = require('axios');
-                const URL = 'http://0.0.0.0:5000'; 
+  export default {
+    name: 'Profile',
+    data() {
+      return {
+        id: '',
+        token: '',
+        res: {}
+      };
+    },
+    mounted() {
+      this.token = localStorage.getItem('user-token')
+      this.id = this.$route.params.id
+      const axios = require('axios');
+      axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`
+      console.log(this.id);
+      axios
+        .get('http://0.0.0.0:5000/users/' + this.id)
+        .then(response => (
+          this.res = response.data,
+            console.log(JSON.stringify(response))
+        ))
+        .catch(error => (console.log(error)));
+    },
+    methods: {
+      getBase64(file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+          return reader.result;
+        };
+        reader.onerror = function (error) {
+          console.log('Error: ', error);
+        };
+      },
+      uploadImage(event) {
+        const axios = require('axios');
+        const URL = 'http://0.0.0.0:5000';
 
-                let file = event.target.files[0]; 
-
-                let config = {
-                header : {
-                    'Content-Type' : 'image/png'
-                    }
-                }
-
-                //axios post here
-                
-            }
+        let file = event.target.files[0];
+        const fileBase64 = this.getBase64(file);
+        let config = {
+          header: {
+            'Content-Type': 'image/png'
+          }
         }
+
+        //axios post here
+
+      }
     }
+  }
 </script>
 
 <style scoped>
